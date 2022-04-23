3e37a2ad12f41ac581d12de8f48fb7d12550b2ec
Shabalin Sergey <sergeyshm38@gmail.com>
2022-04-23 14:08:31 +0300

    First

---
+++ src/Account_add.java
@@ -0,0 +1,24 @@
+public class Account_add {
+    public static void main(String[] args) {
+
+        int Clientaccount = 100; // Сумма на счете клиента
+        int Accountadd = 200; // Сумма пополнения
+        int Bonus = 0;// Бонусные рубли
+        boolean replenishes = true;
+
+        int Sum = Clientaccount + Accountadd;
+        Clientaccount = Sum;
+
+        if (Accountadd > 100) {
+            Bonus = Accountadd / 100;
+        } else {
+
+        }
+        // Условным оператором проверяете превысила ли
+        // сумма пополнения порог и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+        System.out.println(Clientaccount + " Счет клиента ");
+        System.out.println(Bonus + " Бонусы ");
+    }
+}
