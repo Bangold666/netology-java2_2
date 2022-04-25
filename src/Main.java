6b06ae629c2c01298f69cb2f77b8532f13555444
Shabalin Sergey <sergeyshm38@gmail.com>
2022-04-25 18:03:57 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int Clientaccount = 100; // Сумма на счете клиента
-        int Accountadd = 200; // Сумма пополнения
-        int Bonus = 0;// Бонусные рубли
+        int clientAccount = 100; // Сумма на счете клиента
+        int accountAdd = 2000; // Сумма пополнения
+        int bonus = 0;// Бонусные рубли
         boolean replenishes = true;
 
-        int Sum = Clientaccount + Accountadd;
-        Clientaccount = Sum;
+        int sum = clientAccount + accountAdd;
+        clientAccount = sum;
 
-        if (Accountadd > 100) {
-            Bonus = Accountadd / 100;
+        if (accountAdd > 1000) {
+            bonus = accountAdd / 100;
         } else {
 
         }
@@ -18,7 +18,7 @@ public class Main {
         // сумма пополнения порог и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
-        System.out.println(Clientaccount + " Счет клиента ");
-        System.out.println(Bonus + " Бонусы ");
+        System.out.println(clientAccount + " Счет клиента ");
+        System.out.println(bonus + " Бонусы ");
     }
 }
